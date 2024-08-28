59bcfe47f39a8774d81803e04eef4c567185f3fa
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-08-28 18:51:51 +0100

    first commit task7 add type annotation

--- app/models/RepositoryModel.scala
+++ app/models/RepositoryModel.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json.{Json, OFormat}
 
 case class RepositoryModel(name:String,`type`: String ,sha:String){
-  val repositoryModelDTO = RepositoryModelDTO(name,`type`,sha)
+  val repositoryModelDTO: RepositoryModelDTO = RepositoryModelDTO(name,`type`,sha)
 }
 
 object RepositoryModel{
